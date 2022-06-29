1a9d787b6e931f51f106d7481ef26bcafb3ede2b
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-29 18:23:50 -0400

    Clase 11: Webpack Alias

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,12 @@ module.exports = {
   },
   resolve: {
     extensions: [".js"],
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    },
   },
   module: {
     rules: [
@@ -47,7 +53,7 @@ module.exports = {
             mimetype: "application/font-woff",
             name: "[name].[contenthash].[ext]",
             outputPath: "./assets/fonts/",
-            publicPath: "./assets/fonts/",
+            publicPath: "../assets/fonts/",
             esModule: false,
           },
         },
