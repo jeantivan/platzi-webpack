a5f9ee5c0f2eba8b7ee6519bfea6a4985b62251b
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-29 18:09:34 -0400

    Clase 10: Optimización: hashes, compresión y minificación de archivos

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   // Punto de entrada del proyecto
@@ -9,7 +11,7 @@ module.exports = {
   // Punto de salida del proyecto
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
@@ -36,13 +38,14 @@ module.exports = {
         type: "asset/resource",
       },
       {
+        // Reglas para reconocer fuentes en formato woff y woff2
         test: /\.(woff|woff2)$/,
         use: {
           loader: "url-loader",
           options: {
             limit: 10000,
             mimetype: "application/font-woff",
-            name: "[name].[ext]",
+            name: "[name].[contenthash].[ext]",
             outputPath: "./assets/fonts/",
             publicPath: "./assets/fonts/",
             esModule: false,
@@ -57,7 +60,9 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].[contenthash].css",
+    }),
     new CopyPlugin({
       patterns: [
         {
@@ -67,4 +72,8 @@ module.exports = {
       ],
     }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
 };
