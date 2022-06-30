738420dae7e4d0c0edb506aecc077e7297bff3e5
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-30 13:42:05 -0400

    Clase 12:  Variables de entorno

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-
+const Dotenv = require("dotenv-webpack");
 module.exports = {
   // Punto de entrada del proyecto
   entry: "./src/index.js",
@@ -77,6 +77,7 @@ module.exports = {
         },
       ],
     }),
+    new Dotenv()
   ],
   optimization: {
     minimize: true,
