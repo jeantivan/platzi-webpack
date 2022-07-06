1439b1cea655a8b10ce4131f5eba0a8a48d48aa2
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-06 17:16:33 -0400

    Clase 16: Webpack Dev Server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,7 +14,6 @@ module.exports = {
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   mode: "development",
-  watch: true, // Observa los cambios en el proyecto y rebuildea
   resolve: {
     extensions: [".js"],
     alias: {
@@ -80,4 +79,15 @@ module.exports = {
     }),
     new Dotenv(),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+      watch: true,
+    },
+    watchFiles: path.join(__dirname, "./**"), //observa los cambios en todos nuestros archivos y actualiza el navegador
+    compress: true,
+    historyApiFallback: true,
+    port: 4000,
+    open: true,
+  },
 };
